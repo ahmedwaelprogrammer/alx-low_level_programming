eb938820c52e7efa1dbf70fb1295dba56ffc5976
ahmed <aw88301@gmail.com>
2023-06-16 14:07:45 -0700

    6

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Desciption: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive", n);
+
+	else if (n < 0)
+		printf("%d is negative", n);
+
+	else
+		printf("%d is zero", n);
+
+	return (0);
+}
