0cde0e811407efed1b31d493e26a00ee4591dc58
ahmed <aw88301@gmail.com>
2023-06-15 19:45:04 -0700

    6

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,8 @@
 
 int main(void)
 {
-	fputs("and that piece of art is useful\"
-	 - Dora Korpar, 2015-10-19\n", stderr);
+	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+
+	fputs(text, stderr);
 	return (1);
 }
