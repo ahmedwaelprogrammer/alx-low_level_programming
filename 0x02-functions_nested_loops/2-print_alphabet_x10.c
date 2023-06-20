92906616c8dc0dba5fe7ccdf6a82225127c2753f
ahmed <aw88301@gmail.com>
2023-06-20 12:39:54 -0700

    6

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - print alphabet lowercase
+ * print_alphabet_x10 - print alphabet
  *
  * Return: Always 0.
  */
@@ -11,7 +11,8 @@ void print_alphabet_x10(void)
 	char c = 'a';
 	int count = 0;
 
-	while (count < 10){
+	while (count < 10)
+	{
 
 	while (c <= 'z')
 	{
