3c12337c383aedb22063d6f43b46d75b7891fd98
ahmed <aw88301@gmail.com>
2023-06-22 12:05:52 -0700

    6

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,19 +1,17 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point
- *
- * Desciption: A C program that prints with put function
- *
- * Return: Always 0 (Success)
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
 {
-        int i;
+	int i;
 
 	i = 0;
 
 	positive_or_negative(i);
-        return (0);
+
+	return (0);
 }
