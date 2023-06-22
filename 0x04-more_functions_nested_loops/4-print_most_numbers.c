3eb5578d923e064dd1c2ca70a816ee0ba7a80713
ahmed <aw88301@gmail.com>
2023-06-22 11:29:28 -0700

    6

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - entry point
+ *
+ * Description: Prints the numbers with _putchar
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	for (int i = '0'; i <= '9'; i++)
+	{
+		if (i == '2' || i == '4')
+		{
+			i++;
+		}
+		_putchar(i);
+	}
+	_putchar('\n');
+}
