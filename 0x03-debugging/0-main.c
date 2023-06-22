cd1843d1abe2ec7ec80558f9a10ce67140b0da78
ahmed <aw88301@gmail.com>
2023-06-21 20:28:38 -0700

    6

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Desciption: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+        return (0);
+}
