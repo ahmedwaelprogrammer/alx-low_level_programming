981bd5e978fd5f9a2942c97484496d5aee9426c2
ahmed <aw88301@gmail.com>
2023-06-26 05:03:30 -0700

    6

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ *reset_to_98 - updates a integer value to 98
+ *@n: A pointer to an int that will be updated
  *
- * Desciption: A C program that prints with put function
- *
- * Return: Always 0 (Success)
+ *Return: void that means is correct
  */
 
 void reset_to_98(int *n)
