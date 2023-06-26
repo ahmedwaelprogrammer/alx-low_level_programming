ec5d039811db3dc025c40e7e59d9c1d8ac5f4e50
ahmed <aw88301@gmail.com>
2023-06-26 10:24:13 -0700

    6

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+void puts_half(char *str)
+{
+	int i = strlen(str)/2;
+	int ln = strlen(str);
+
+	while (i < ln+1)
+	{
+		if (str[i] == '\0')
+		{
+			break;
+		}
+	putchar(str[i]);
+	i++;
+	}
+}
