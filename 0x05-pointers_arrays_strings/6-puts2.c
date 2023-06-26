52b12778f274b8ff501bc32ab1cd3a7f352e80c6
ahmed <aw88301@gmail.com>
2023-06-26 08:55:50 -0700

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *puts2 - prints every character of a string
+ *@str: A pointer to an int that will be changed
+ *
+ *Return: void which means it is correct
+ */
+
+void puts2(char *str)
+{
+	_putchar(str[0]);
+}
