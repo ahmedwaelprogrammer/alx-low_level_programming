3a6d0479d21416e307f2faa27029a3ba6f4faebf
ahmed <aw88301@gmail.com>
2023-06-26 08:21:16 -0700

    6

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - prints a string in reverse order
+ *@s: A pointer to an int that will be changed
+ *
+ *Return: void which means our answer is correct
+ */
+
+void print_rev(char *s)
+{
+
+	for(int i = strlen(*s + 1); i >= 0; i--)
+	{
+		char lit = *s[i];
+		putchar(lit);
+		lit = "";
+	}
+	putchar('\n');
+}
